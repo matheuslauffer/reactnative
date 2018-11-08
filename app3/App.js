ed5679ac9dc55344093179fe5efab3b4ae2605ea
Matheus Lauffer <matheuslauffer1@gmail.com>
2018-11-07 23:34:12 -0200

    Criação do app3

--- app3/App.js
+++ app3/App.js
@@ -9,41 +9,21 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+class MeuComponente extends Component{
+  render(){
+    return(
+      <View>
+        <Text>{this.props.propriedade1}</Text>
+        <Text>Segundo Parâmetro</Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+export default class App3 extends Component{
+  render(){
+    return(
+      <MeuComponente propriedade1 = 'Banana'></MeuComponente>
+    );
+  }
+}
