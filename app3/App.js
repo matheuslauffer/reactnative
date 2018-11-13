c824b8a9bd1422ec51b9ba59bcc5a7c810784890
Matheus Lauffer <matheuslauffer1@gmail.com>
2018-11-13 20:26:13 -0200

    criação do app 4

--- app3/App.js
+++ app3/App.js
@@ -7,71 +7,78 @@
  */
 
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { Topo } from './src/components/topo';
 import { Icone } from './src/components/icone';
 
 
 export default class App3 extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = { escolhaUsuario : '', escolhaComputador : '', resultado : '' }
+    this.state = { escolhaUsuario: props.escolhaUsuario, 
+                    escolhaComputador: props.escolhaComputador, 
+                    resultado: props.resultado
+                  };
   }
 
-  jokenpo(escolhaUsuario){
-    var numAleatorio = Math.floor(Math.random()*3);
-    var escolhaComputador = '';
-    switch(numAleatorio){
+  jokenpo(escolhaUsuario) {
+    const numAleatorio = Math.floor(Math.random() * 3);
+    let escolhaComputador = '';
+    switch (numAleatorio) {
       case 0: escolhaComputador = 'pedra'; break;
       case 1: escolhaComputador = 'papel'; break;
       case 2: escolhaComputador = 'tesoura'; break;
+      default : escolhaComputador = '';
     }
-    var resultado = '';
-    if(escolhaComputador == 'pedra'){
-      if(escolhaUsuario == 'pedra')
+    let resultado = '';
+    if (escolhaComputador === 'pedra') {
+      if (escolhaUsuario === 'pedra') {
         resultado = 'Empate';
-      else if(escolhaUsuario == 'papel')
+      } else if (escolhaUsuario === 'papel') {
         resultado = 'Você ganhou';
-      else
+      } else {
         resultado = 'Você perdeu';
-    }else if(escolhaComputador == 'papel'){
-      if(escolhaUsuario == 'pedra')
+      }
+    } else if (escolhaComputador === 'papel') {
+      if (escolhaUsuario === 'pedra') {
         resultado = 'Você prdeu';
-      else if(escolhaUsuario == 'papel')
+      } else if (escolhaUsuario === 'papel') {
         resultado = 'Empate';
-      else
+      } else {
         resultado = 'Você ganhou';
-    }else{
-      if(escolhaUsuario == 'pedra')
+      }
+    } else {
+      if (escolhaUsuario === 'pedra') {
         resultado = 'Você ganhou';
-      else if(escolhaUsuario == 'papel')
+      } else if (escolhaUsuario === 'papel') {
         resultado = 'Você perdeu';
-      else
+      } else {
         resultado = 'Empate';
+      }
     }
-    this.setState({escolhaUsuario : escolhaUsuario, escolhaComputador : escolhaComputador, resultado : resultado});
+    this.setState({ escolhaUsuario, escolhaComputador, resultado });
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <View>
-        <Topo></Topo>
+        <Topo />
         <View style={styles.painelAcoes}>
           <View style={styles.btnEscolha}>
-            <Button title='Pedra' onPress={() => {this.jokenpo('pedra')}}/>
+            <Button title='Pedra' onPress={() => { this.jokenpo('pedra') }} />
           </View>
           <View style={styles.btnEscolha}>
-            <Button title='Papel' onPress={() => {this.jokenpo('papel')}}/>
+            <Button title='Papel' onPress={() => { this.jokenpo('papel') }} />
           </View>
           <View style={styles.btnEscolha}>
-            <Button title='Tesoura' onPress={() => {this.jokenpo('tesoura')}}/>
+            <Button title='Tesoura' onPress={() => { this.jokenpo('tesoura') }} />
           </View>
         </View>
         <View style={styles.palco}>
           <Text style={styles.txtResultado}>{this.state.resultado}</Text>
 
-          <Icone escolha={this.state.escolhaComputador} jogador='computador'></Icone>
-          <Icone escolha={this.state.escolhaUsuario} jogador='usuario'></Icone>
+          <Icone escolha={this.state.escolhaComputador} jogador='computador' />
+          <Icone escolha={this.state.escolhaUsuario} jogador='usuario' />
         </View>
       </View>
     );
@@ -79,22 +86,22 @@ export default class App3 extends Component {
 }
 
 const styles = StyleSheet.create({
-  btnEscolha : {
-    width : 90
+  btnEscolha: {
+    width: 90
   },
-  painelAcoes : {
-    flexDirection : 'row',
-    justifyContent : 'space-between',
-    margin : 10
+  painelAcoes: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    margin: 10
   },
-  palco : {
-    alignItems : 'center',
-    marginTop : 10
+  palco: {
+    alignItems: 'center',
+    marginTop: 10
   },
-  txtResultado : {
-    fontSize : 25,
-    fontWeight : "bold",
-    color : 'red',
-    height : 60
+  txtResultado: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: 'red',
+    height: 60
   }
 });
